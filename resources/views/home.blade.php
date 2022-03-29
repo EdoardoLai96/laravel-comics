eae9cf4c66f0236b78ee42407fb235d400b7bd71
Edoardo Lai <edoardolai96@gmail.com>
2022-03-29 21:59:38 +0200

    Aggiunta cta tra main e footer di /home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,6 +22,30 @@
     <div class="load_more">
         LOAD MORE
     </div>
-</main class="main_home">
+    <div class="cta">
+        <ul class="nav">
+            <li>
+                <img src="/img/buy-comics-digital-comics.png" alt="">
+                <a href="">DIGITAL COMICS</a>
+            </li>
+            <li>
+                <img src="/img/buy-comics-merchandise.png" alt="">
+                <a href="">DC MERCHANDISE</a>
+            </li>
+            <li>
+                <img src="/img/buy-comics-subscriptions.png" alt="">
+                <a href="">SUBSCRIPTION </a>
+            </li>
+            <li>
+                <img src="/img/buy-comics-shop-locator.png" alt="">
+                <a href="">COMIC SHOP LOCATOR</a>
+            </li>
+            <li>
+                <img id="power-visa" src="/img/buy-dc-power-visa.svg" alt="">
+                <a href=""> DC POWER VISA</a>
+            </li>
+        </ul>
+    </div>
+</main>
 
 @endsection
